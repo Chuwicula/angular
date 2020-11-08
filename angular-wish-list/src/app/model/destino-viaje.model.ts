babc9a9b220bc7b8f3c8e4c5713035d144814be1
Nicolas NCR. Cabrejo Rubiano <ncabrejo@FONACCCOL.ORG>
2020-11-08 14:37:09 -0500

    segunda version

---
+++ angular-wish-list/src/app/model/destino-viaje.model.ts
@@ -0,0 +1,9 @@
+export class DestinoViaje{
+	nombre:string;
+	imagenUrl:string;
+	constructor(nombreIn:string,
+	imagenUrlIn:string){
+		this.nombre = nombreIn;
+		this.imagenUrl = imagenUrlIn;
+	}
+}
